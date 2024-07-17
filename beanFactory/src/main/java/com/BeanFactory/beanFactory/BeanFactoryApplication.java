2ceceb7daf75686f5562d1ec0bbf3394346c054e
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-07-17 23:23:05 +0530

    Add files via upload

--- beanFactory/src/main/java/com/BeanFactory/beanFactory/BeanFactoryApplication.java
+++ beanFactory/src/main/java/com/BeanFactory/beanFactory/BeanFactoryApplication.java
@@ -1,13 +1,31 @@
 package com.BeanFactory.beanFactory;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class BeanFactoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeanFactoryApplication.class, args);
+		ApplicationContext fact = SpringApplication.run(BeanFactoryApplication.class, args);
+		Student student3 = (Student) fact.getBean("student");
+
+//		used BeanFactory
+		BeanFactory beanFactory = new ClassPathXmlApplicationContext("bean-factory-demo.xml");
+		Student student1 = (Student) beanFactory.getBean("student");
+
+//		used ApplicationContext
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean-factory-demo.xml");
+		Student student2 = (Student) applicationContext.getBean("student");
+
+
+
+		System.out.println(student1);
+		System.out.println(student2);
+		student3.say();
 	}
 
 }
