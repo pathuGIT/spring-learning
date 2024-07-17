6413a2ba9fbc5d14134112fae54e2b746e53131e
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-07-17 20:58:27 +0530

    Add files via upload

---
+++ beanFactory/src/main/java/com/BeanFactory/beanFactory/BeanFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.BeanFactory.beanFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanFactoryApplication.class, args);
+	}
+
+}
