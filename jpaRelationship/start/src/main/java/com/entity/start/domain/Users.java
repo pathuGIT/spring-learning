707b69fe25ea4517abb378593a6def7acdabccd2
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-07-20 21:45:44 +0530

    Spring Jpa Relationship

---
+++ jpaRelationship/start/src/main/java/com/entity/start/domain/Users.java
@@ -0,0 +1,35 @@
+package com.entity.start.domain;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String name;
+
+    @Column(length = 255, nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "users", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Post>posts;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
