7123ef89dcdda0b52c1899c113c28616f7df97a0
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-07-23 12:06:04 +0530

    Add files via upload

---
+++ Initializing-List/src/test/java/com/wak/Initializing_List/InitializingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wak.Initializing_List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
